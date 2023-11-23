db48a0492172753b3f06e185b4e7df795930b142
Onischenko Kostyantyn <Onischenko411@gmail.com>
2023-11-23 23:34:51 +0200

    1 file added

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-const time = prompt(`Задайте кількість годин поки ви будете відсутні?`)
-let x = 3600
-let y = time * x
-alert(`${y} таку кількість секунд ви будете відсутні!`)
+const userEntersAction = prompt ('Введите действие которое хотите сделать ( - , + ,* , / )');
+const numberOne = +prompt ('Введите первое число');
+const numberTwo = +prompt ('Введите второе число');
+ const add = `+`;
+// const mult = `*`;
+// const sub = `-`
+// const div = `/`
+// const action = add || mult || sub || div;
+let resultAdd = numberOne + numberTwo
+let resultDiv = numberOne / numberTwo
+let resultSub = numberOne - numberTwo
+let resultMult = numberOne * numberTwo
+
+switch (userEntersAction){
+    case '*':
+        alert(`${numberOne} ${userEntersAction} ${numberTwo} = ${resultMult}`
+        )
+        break
+    case '/':
+        alert(`${numberOne} ${userEntersAction} ${numberTwo} = ${resultDiv}`)
+        break
+    case '-':
+        alert(`${numberOne} ${userEntersAction} ${numberTwo} = ${resultSub}`
+        )
+        break
+    case '+':
+        alert(`${numberOne} ${userEntersAction} ${numberTwo} = ${resultAdd}`)
+        break
+}
+console.log(`${userEntersAction}`)
